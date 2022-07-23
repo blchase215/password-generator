c2ab5407c5de53f91ec6cbdc6b856d5294315ddc
Brian Chase <brianchasemusic@gmail.com>
2022-07-23 05:33:22 -0400

    refining code and checking functionality, only final generation remaining

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,81 @@
 var generateBtn = document.querySelector('#generate');
 
 function generatePassword() {
-  var password = 'password';
-  // TODO: add code to generate the password here
+  
+  var password = '';
+  var alphaLowers = "abcdefghijklmnopqrstuvwxyz";
+  var alphaUppers = alphaLowers.toUpperCase();
+  var numeric = "0123456789";
+  var specChar = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+  
+  // array of character sets
+  // undesired character sets 
+  var choiceVar = [alphaLowers, alphaUppers, numeric, specChar];
 
-  return password;
-}
+  // store value of unwanted characters
+  var notChosen = [];
+  
+// User chooses password length...
+  
+  var userLength = window.prompt("Choose a Password length between 8 and 128 characters.", "8 - 128");
+  
+// 0 is entered
+
+  if (userLength == 0) {
+    password = "William Corgan is a Zero, \nbut your password shouldn't be.\nTry again.";
+    return password;
+  };
+
+  if (userLength < 0) {
+    password = "A password can not be created from thin air!\nTry again.";
+    return password;
+  };
+  
+// Confirm length conforms to parameter limits
+
+  if (userLength < 8 || userLength > 128) {
+    password = "Please choose a length between 8 and 128 characters.";
+    return password;
+  };
+
+// Character set inclusion prompts
+  
+  var lowers = window.prompt("Use 'lowercase' letters?");
+  var UPPERS = window.prompt("USE 'UPPERCASE' LETTERS?");
+  var num = window.prompt("Use Numerical values 0 - 9?");
+  var spec = window.prompt(" !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~");
+  
+// remove if false
+
+  if (!lowers) {
+     var not = choiceVar.shift();
+     notChosen.push(not);
+  }; 
+
+  if (!UPPERS) {
+     var not = choiceVar.shift();
+     notChosen.push(not);
+  };
+
+  if (!num) {
+     var not = choiceVar.shift();
+     notChosen.push(not);
+  } 
+  if (!spec) {
+     var not = choiceVar.shift();
+     notChosen.push(not);
+  }; 
+
+console.log(notChosen);
+
+  if (notChosen.length === 4) {
+    password = "Please choose at least one type of character to include.";
+    return password;
+  } else {
+    
+  };
+  return "'W.I.P'\n'Work in Progress'\nby Cardi B and Megan Thee Stallion";
+};
 
 // Write password to the #password input
 function writePassword() {
@@ -17,35 +87,40 @@ function writePassword() {
 }
 
 // Add event listener to generate button
+function clearBox() {
+  var passBox = document.querySelector('#password');
+  passBox.value = "Generating...";
+};
+
+generateBtn.addEventListener('click', clearBox);
 generateBtn.addEventListener('click', writePassword);
 
 
-// psuedocode
 
-var passlength = '';
-var alphaLowers = "abcdefghijklmnopqrstuvwxyz";
-var alphaUppers = alphaLowers.toUpperCase();
-var numeric = "0123456789";
-var specChar = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
-console.log(alphaLowers.length);
-console.log(alphaUppers.length);
-console.log(numeric.length);
-console.log(specChar.length);
-console.log(alphaLowers);
-console.log(alphaUppers);
-console.log(numeric);
-console.log(specChar);
+// pseudocode
 
 
+// console.log(alphaLowers.length);
+// console.log(alphaUppers.length);
+// console.log(numeric.length);
+// console.log(specChar.length);
+// console.log(alphaLowers);
+// console.log(alphaUppers);
+// console.log(numeric);
+// console.log(specChar);
+
+// loop to test string iteration
 var checkLoop = function (set) {
   for (i = 0; i < set.length; i++) {
     console.log(set[i]);
   };
 };
-var checkRandomLoop = function (set) {
-  for (i = 0; i < set.length; i++) {
-    var randomPick = Math.floor(Math.random() * set.length);
-    console.log(randomPick);
+
+// test for random choice iteration
+var checkRandomLoop = function (list) {
+  var randomPick = Math.floor(Math.random() * list.length);
+  for (i = 0; i < list.length; i++) {
+    
   };
   console.log(randomPick.length);
   return randomPick;
@@ -58,6 +133,7 @@ var checkRandomLoop = function (set) {
 // select criteria
 
 // user clicks button
+
 // prompt with password criteria
 // display choices in #password element, separated by comma
 // true choices display charSet name
@@ -102,17 +178,12 @@ var checkRandomLoop = function (set) {
 // add string variable 
 // of each choice to choiceVar
 // iterate through with:
-var choiceVar = [];
-choiceVar.push(alphaLowers, alphaUppers, numeric, specChar);
-console.log(choiceVar);
-var randomChoiceLoop = function (set) {
-  for (i = 0; i < set.length; i++) {
-    var randomPick = Math.floor(Math.random() * set.length);
-    console.log(randomPick);
-  };
-  console.log(randomPick.length);
-  return randomPick;
-}; 
+
+// var randomChoiceLoop = function (set) {
+//   for (i = 0; i < passLength; i++) {
+//     var randomPick = Math.floor(Math.random() * set.length);
+//   };
+// }; 
 // 
 // no character set options chosen message
 //
