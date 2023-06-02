e9032beb4ee4b9a4a2567c48f54dbc5a64333a92
Brian Chase <brianchasemusic@agmail.com>
2023-06-02 16:07:46 -0400

    loop function working for all charsets

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,23 @@
-const looper = function(x,y) {
-    for (i = 0; i < x.length; i++) {
-        solution = solution + y[Math.floor(Math.random * x.length)];
-        return solution;
+var userChoice = [];
+var alphaLowers = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var alphaUppers = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var numericChars = ["0","1","2","3","4","5","6","7","8","9"];
+var specialChars = [" ","!","\"","#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","\\","]","^","_","`","{","|","}","~",];
+
+// looper function takes
+// x - array to loop through
+// y - length of users passord choice, or length of individual array
+
+const arrayLooper = function(x,y) {
+    solution = [];
+    
+    for (i = 0; i < y; i++) {
+        solution.push( x[ Math.floor( Math.random() * x.length ) ] );
     }
+    return solution;
 }
+
+console.log(arrayLooper(alphaLowers,20));
+console.log(arrayLooper(alphaUppers,20));
+console.log(arrayLooper(numericChars,20));
+console.log(arrayLooper(specialChars,20));
