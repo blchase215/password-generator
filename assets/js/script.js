ce3c1e467fc178d70a3bb5c38519e5f7749211d9
Brian Chase <brianchasemusic@agmail.com>
2023-08-27 14:34:05 -0400

    tidying up

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,21 @@
-var userChoice = [];
-var alphaLowers = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var alphaUppers = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-var numericChars = ["0","1","2","3","4","5","6","7","8","9"];
-var specialChars = [" ","!","\"","#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","\\","]","^","_","`","{","|","}","~",];
+let userChoice = "";
+const alphaLowers = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+const alphaUppers = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+const numericChars = ["0","1","2","3","4","5","6","7","8","9"];
+const specialChars = [" ","!","\"","#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","\\","]","^","_","`","{","|","}","~",];
 
-// looper function takes
+// random looper function takes
 // x - array to loop through
 // y - length of users passord choice, or length of individual array
 
-const arrayLooper = function(x,y) {
-    solution = [];
+const randomArrayLooper = function(x,y) {
+    let solution = [];
     
     for (i = 0; i < y; i++) {
-        solution.push( x[ Math.floor( Math.random() * x.length ) ] );
+        solution = solution.concat( x[ Math.floor( Math.random() * x.length ) ] );
     }
     return solution;
 }
 
-console.log(arrayLooper(alphaLowers,20));
-console.log(arrayLooper(alphaUppers,20));
-console.log(arrayLooper(numericChars,20));
-console.log(arrayLooper(specialChars,20));
+console.log(userChoice = [...alphaLowers,...alphaUppers,...numericChars,...specialChars]);
+console.log(randomArrayLooper(userChoice,30));
